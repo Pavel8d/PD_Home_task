738af7ae8386cb850cbbc4081cdbed07d183c841
Pavel8d <paveldenisov8@yandex.ru>
2022-06-08 00:09:03 +0300

    Add exercise 5

---
+++ src/by/tms/lesson2/lesson2GitTest.java
@@ -0,0 +1,14 @@
+package by.tms.lesson2;
+
+public class lesson2GitTest {
+
+    public static void main(String[] args) {
+        int i = 5668485;
+        double ii = 5668485;
+        System.out.print(i / 60);
+        System.out.println(" min");
+        System.out.print(ii / 60);
+        System.out.println(" min");
+
+    }
+}
